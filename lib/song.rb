4e82283fae6c4bf93ef20b9b540feb84f4bc0adc
jmoypace <jmoypace@gmail.com>
2018-07-17 17:51:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,5 +10,8 @@ def self.new_by_filename(filename)
     new_song.artist_name = artist
     new_song
   end
-
+  def artist_name=(name)
+      self.artist = Artist.find_or_create_by_name(name)
+      artist.add_song(self)
+    end
 end

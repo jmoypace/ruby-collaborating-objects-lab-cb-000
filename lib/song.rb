5f42f6558b2ea4ce83f39d09b4f675c11057f5bf
jmoypace <jmoypace@gmail.com>
2018-07-17 17:14:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,5 @@ def initialize(name)
 end
 
 
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
+
 end

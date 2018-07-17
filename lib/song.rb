4aa49326daccfbeddd83ca00040e58c7cae3de88
jmoypace <jmoypace@gmail.com>
2018-07-17 17:50:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,11 @@ def initialize(name)
   @name=name
 end
 
-
+def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
 
 end
